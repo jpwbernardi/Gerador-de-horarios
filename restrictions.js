0e8154dd73f897a0d44b087b538b179b5b546cf5
Gabriel Galli <g7.galli96@gmail.com>
2016-08-18 15:05:23 -0300

    Oh, nevermind

--- restrictions.js
+++ restrictions.js
@@ -2,3 +2,6 @@
 // https://github.com/kripken/sql.js
 // sqlite.org
 // http://node-modules.com/search?u=&q=sqlite
+
+// https://io2015codelabs.appspot.com/codelabs/lovefield#1
+// https://google.github.io/lovefield/
