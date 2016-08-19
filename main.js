414f0deabff0f6626286355c1d002868ff7ae9ff
Gabriel Galli <g7.galli96@gmail.com>
2016-08-18 22:39:22 -0300

    SQLite3 tests done. db global var available

--- main.js
+++ main.js
@@ -1,52 +1,70 @@
-const electron = require('electron')
+const electron = require('electron');
+const sqlite3 = require('sqlite3').verbose();
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function() {
+  global.db = new sqlite3.Database('scheduler.db', function(err) {
+    console.log("Error opening the database: " + err);
+  });
+  global.db.on('open', function() {
+    console.log('Dabase opened successfully!');
+  });
+  global.db.on('close', function() {
+    console.log('Dabase closed successfully!');
+  });
+  createWindow();
+})
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
+})
+
+app.on('quit', function() {
+  global.db.close(function(err) {
+    console.log("Error closing the database: " + err);
+  });
 })
 
 // In this file you can include the rest of your app's specific main process
--- restrictions.js
+++ restrictions.js
@@ -1,25 +1,53 @@
-// https://github.com/mapbox/node-sqlite3
-// https://github.com/kripken/sql.js
-// sqlite.org
-// http://node-modules.com/search?u=&q=sqlite
-
-// https://io2015codelabs.appspot.com/codelabs/lovefield#1
-// https://google.github.io/lovefield/
-var sqlite3 = nodeRequire('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
+function init(db) {
+  db.serialize(function() {
+    var stmt = null;
+    db.parallelize(function() {
+      db.run("create table professor(name varchar(64), nickname varchar(32), primary key(name));", function(err) {
+        console.log("Professor error: " + err);
+        if (err === null) {
+          stmt = db.prepare("insert into professor values (?, ?)");
+          stmt.run("Graziela Simone Tonin", "Grazi");
+          stmt.run("Claunir Pavan", "Pavan");
+          stmt.run("José Bins", "Bins");
+          stmt.run("Bráulio de Mello", "Bráulio");
+          stmt.finalize();
+        }
+      });
+      db.run("create table subject(code varchar(8), title varchar(64), primary key(code));", function(err) {
+        console.log("Subject error: " + err);
+        if (err === null) {
+          stmt = db.prepare("insert into subject values (?, ?)");
+          stmt.run("GCH008", "Iniciação à prática científica");
+          stmt.run("GEX103", "Engenharia de software II");
+          stmt.run("GEX105", "Redes de computadores");
+          stmt.run("GEX107", "Computação gráfica");
+          stmt.run("GEX108", "Construção de compiladores");
+          stmt.finalize();
+        }
+      });
+    });
+    db.run("create table link(name varchar(64), code varchar(8), primary key(name, code), foreign key(name) references professor(name), foreign key(code) references subject(code));", function(err) {
+      console.log("Link error: " + err);
+      if (err === null) {
+        stmt = db.prepare("insert into link values (?, ?)");
+        stmt.run("Graziela Simone Tonin", "GCH008");
+        stmt.run("Graziela Simone Tonin", "GEX103");
+        stmt.run("Claunir Pavan", "GEX105");
+        stmt.run("José Bins", "GEX107");
+        stmt.run("Bráulio de Mello", "GEX108");
+        stmt.finalize();
+      }
+    });
+    stmt = null;
   });
-});
+}
 
-db.close();
+const db = nodeRequire('electron').remote.getGlobal('db');
+init(db);
+// db.each("select * from link natural join professor natural join subject",
+// function(err, row) {
+//   console.log("Professor " + row.name + " (a.k.a. " + row.nickname + ") teaches the subject " + row.title + " (" + row.code + ")");
+// },
+// function(err, nrows) {
+//   console.log("Completion error: " + err + "; number of rows retrieved: " + nrows);
+// });
