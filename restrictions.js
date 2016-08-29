3d15d0fc30dc4f7bbd54001d4c046ec05b27e240
Gabriel Galli <g7.galli96@gmail.com>
2016-08-28 22:28:41 -0300

    Saving professor_restrictions

--- restrictions.js
+++ restrictions.js
@@ -3,32 +3,42 @@ const db = nodeRequire('electron').remote.getGlobal('db');
 
 $(".autocomplete").autocomplete({
   minLength: 0,
+  autoFocus: true,
   source: function(request, response) {
     var results = [];
-    var tis = this.element[0];
-    var table = tis.getAttribute("id");
+    var $tis = $(this.element[0]);
+    var table = $tis.attr("id");
     var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
-    db.each("select * from " + table, function(err, row) {
-      var i = 0;
-      var pk = null, label = "";
-      $.each(row, function(key, value) {
-        if (i === 0) {
-          pk = value;
-        }
-        else {
-          if (i > 1) label += " ";
-          label += value;
-        }
-        i++;
-      });
-      label += " (" + pk + ")";
-      if (matcher.test(label)) {
+    var query = "select ";
+    var field = $tis.attr("select-field"),
+      from = $tis.attr("select-from"),
+      group = $tis.attr("select-group-by"),
+      order = $tis.attr("select-order-by");
+    if (typeof field !== typeof undefined && field !== "") query += field;
+    else query += "*";
+    query += " from ";
+    if (typeof from !== typeof undefined && from !== "") query += from;
+    else query += table;
+    if (typeof group !== typeof undefined && group !== "") query += " group by " + group;
+    if (typeof order !== typeof undefined && order !== "") query += " order by " + order;
+    console.log(query);
+    db.each(query, function(err, row) {
+      var keys = Object.keys(row);
+      var pk = row[keys[0]], label = "";
+      if (keys.length === 1)
+        label = row[keys[0]];
+      else {
+        label += row[keys[1]];
+        for (let i = 2; i < keys.length; i++)
+          label += " " + row[keys[i]];
+        label += " (" + pk + ")";
+      }
+      if (matcher.test(label))
         results.push({
           id: pk,
-          target: table + "Id",
+          target: $tis.attr("target"),
           value: label
         });
-      }
     }, function(err, nrows) {
       response(results);
     });
@@ -38,15 +48,46 @@ $(".autocomplete").autocomplete({
   }
 });
 
+$(".autocomplete").change(function(event) {
+  if (event.currentTarget.value === "") {
+    $("#" + event.currentTarget.getAttribute("target")).val("");
+  }
+});
+
 $(".save").click(function(event) {
   var target = event.currentTarget;
   var table = target.name;
-  var $elem = $("." + table);
-  $elem.each(function(index, el) {
-    console.log(el);
-    if (el.value == "") {
+  var $elems = $("." + table);
+  var correct = true;
+  $elems.each(function(index, el) {
+    if (el.value === "") {
       $("#" + el.getAttribute("from")).addClass("invalid");
       Materialize.toast("O campo '" + el.name + "' é obrigatório", 2000);
+      correct = false;
     }
   });
+  if (correct) {
+    let params = [];
+    let query = "insert into " + table + " values (";
+    let first = true;
+    $elems.each(function(index, el) {
+      if (!first) {
+        query += ", "
+      }
+      query += "?";
+      first = false;
+      console.log(el);
+      params.push(el.value);
+    });
+    query += ")";
+    console.log(query);
+    console.log(params);
+    db.run(query, params, function(err) {
+      if (err !== null) {
+        Materialize.toast(err, 3000);
+      } else {
+        Materialize.toast("Registro salvo com sucesso!", 2000);
+      }
+    });
+  }
 });
